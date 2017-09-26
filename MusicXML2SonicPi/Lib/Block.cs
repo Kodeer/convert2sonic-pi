6d92f1c81ea90c09db0e349b25bfa5a744c6de8b
Kodeer <32293885+Kodeer@users.noreply.github.com>
2017-09-26 13:51:22 +0200

    Add files via upload

---
+++ MusicXML2SonicPi/Lib/Block.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MusicXml.SonicPi.MusicScore.ScorePart
+{
+    public class Block : IBlock
+    {
+        public List<Measure> Measures { get; set; }
+
+        public Block()
+        {
+            Measures = new List<Measure>();
+        }
+    }
+}
---
+++ MusicXML2SonicPi/Lib/IBlock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MusicXml.SonicPi.MusicScore.ScorePart
+{
+    public interface IBlock
+    {
+        /// <summary>
+        /// Measure list
+        /// </summary>
+        List<Measure> Measures { get; set; }
+    }
+}
