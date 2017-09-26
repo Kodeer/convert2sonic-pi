6a7348ef5b066af01a1b341912c1a9ae4117ae86
Kodeer <32293885+Kodeer@users.noreply.github.com>
2017-09-26 13:55:07 +0200

    Add files via upload

---
+++ MusicXML2SonicPi/Lib/Measure.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using MusicXml.SonicPi.MusicScore.ScorePart.MeasureNote;
+
+namespace MusicXml.SonicPi.MusicScore.ScorePart
+{
+    public class Measure : IMeasure
+    {
+        public string Id { get; set; }
+        public string Caption { get; set; }
+        public int Length
+        {
+            get
+            {
+                if (Notes != null)
+                {
+                    return Notes.Sum(p => p.Length);
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        public List<Note> Notes { get; set; }
+
+        public Measure(string id, string caption)
+        {
+            Id = id;
+            Caption = caption;
+            Notes = new List<Note>();
+        }
+    }
+}
